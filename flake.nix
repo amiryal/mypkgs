ef83757bc9c2b9d18ebd51fbe7ffc5ec3683a29a
Amir Yalon <git@please.nospammail.net>
2025-05-18 11:17:49 +0300

    Initial infrastructure

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  inputs = {
+    nixpkgs.url = "nixpkgs";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+    }:
+    let
+      supportedSystems = nixpkgs.lib.platforms.all;
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+      nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; });
+      forAll =
+        f:
+        forAllSystems (
+          system:
+          f {
+            inherit system;
+            pkgs = nixpkgsFor.${system};
+          }
+        );
+    in
+    {
+      formatter = forAll ({ pkgs, ... }: pkgs.nixfmt-rfc-style);
+
+      packages = forAll (
+        { pkgs, ... }:
+        with pkgs;
+        {
+        }
+      );
+    };
+}
