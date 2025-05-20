c183a66b067bc89895bd00b68bc3c7262da82298
Amir Yalon <git@please.nospammail.net>
2025-05-20 08:39:43 +0300

    Make some dependencies optional

--- aqualung.nix
+++ aqualung.nix
@@ -1,34 +1,61 @@
 {
+  lib,
   stdenv,
   autoreconfHook,
   pkg-config,
   fetchFromGitHub,
 
+  # Mandatory
   gtk3,
   libxml2,
 
+  # Optional
+  enableLadspa ? true,
   lrdf,
-  # libcdio, # the wrong one?
+  enableCdda ? true,
+  libcdio,
+  libcdio-paranoia,
+  enableCddb ? true,
   libcddb,
+  enableSrc ? true,
   libsamplerate,
+  enableIfp ? false, # FIXME: find the right dependency if it exists in nixpkgs
+  enableLua ? true,
   lua,
 
+  # File I/O
+  enableSndfile ? true,
   libsndfile,
+  enableFlac ? true,
   flac,
+  enableVorbis ? true,
   libvorbis,
+  enableSpeex ? true,
   liboggz,
   speex,
+  enableMpeg ? true,
   libmad,
+  enableLame ? true,
   lame,
+  enableMod ? true,
   libmodplug,
+  enableMpc ? true,
   libmpcdec,
+  enableMac ? true,
   mac,
+  enableWavpack ? true,
   wavpack,
+  enableLavc ? true,
   ffmpeg,
 
+  # Output
+  enableSndio ? true,
   sndio,
+  enableAlsa ? true,
   alsa-lib,
+  enableJack ? true,
   libjack2,
+  enablePulse ? true,
   pulseaudio,
 }:
 stdenv.mkDerivation {
@@ -38,36 +65,40 @@ stdenv.mkDerivation {
     autoreconfHook
     pkg-config
   ];
-  buildInputs = [
-    gtk3
-    libxml2
+  buildInputs =
+    [
+      gtk3
+      libxml2
+    ]
+    ++ lib.optional enableLadspa lrdf
+    ++ lib.optionals enableCdda [
+      libcdio
+      libcdio-paranoia
+    ]
+    ++ lib.optional enableCddb libcddb
+    ++ lib.optional enableSrc libsamplerate
+    ++ lib.optional enableIfp (throw "unsupported option")
+    ++ lib.optional enableLua lua
 
-    lrdf
-    # libcdio ?
-    libcddb
-    libsamplerate
-    # libifp ?
-    lua
+    ++ lib.optional enableSndfile libsndfile
+    ++ lib.optional enableFlac flac
+    ++ lib.optional enableVorbis libvorbis
+    ++ lib.optionals enableSpeex [
+      liboggz
+      speex
+    ]
+    ++ lib.optional enableMpeg libmad
+    ++ lib.optional enableLame lame
+    ++ lib.optional enableMod libmodplug
+    ++ lib.optional enableMpc libmpcdec
+    ++ lib.optional enableMac mac
+    ++ lib.optional enableWavpack wavpack
+    ++ lib.optional enableLavc ffmpeg
 
-    libsndfile
-    flac
-    libvorbis
-    liboggz
-    speex
-    libmad
-    lame
-    libmodplug
-    libmpcdec
-    mac
-    wavpack
-    ffmpeg
-
-    sndio
-    # soundcard.h ?
-    alsa-lib
-    libjack2
-    pulseaudio
-  ];
+    ++ lib.optional enableSndio sndio
+    ++ lib.optional enableAlsa alsa-lib
+    ++ lib.optional enableJack libjack2
+    ++ lib.optional enablePulse pulseaudio;
   src = fetchFromGitHub {
     owner = "jeremyevans";
     repo = "aqualung";
