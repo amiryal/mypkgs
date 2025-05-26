5b6d41489fc1b9e1521e7eb08dbce4c2c1aa4cb4
Amir Yalon <git@please.nospammail.net>
2025-05-26 09:47:19 +0300

    Make changes to satisfy latest nixpkgs convensions

--- aqualung.nix
+++ aqualung.nix
@@ -5,6 +5,7 @@
   pkg-config,
   fetchFromGitHub,
   fetchpatch,
+  nix-update-script,
 
   # Mandatory
   gtk3,
@@ -20,7 +21,7 @@
   libcddb,
   enableSrc ? true,
   libsamplerate,
-  enableIfp ? false, # FIXME: find the right dependency if it exists in nixpkgs
+  # enableIfp ? true, # FIXME: find the right dependency to support the iRiver iFP driver if it exists in nixpkgs
   enableLua ? true,
   lua,
 
@@ -43,7 +44,7 @@
   enableMpc ? true,
   libmpcdec,
   enableMac ? true,
-  mac,
+  monkeys-audio,
   enableWavpack ? true,
   wavpack,
   enableLavc ? true,
@@ -62,6 +63,8 @@
 stdenv.mkDerivation (finalAttrs: {
   pname = "aqualung";
   version = "2.0";
+  strictDeps = true;
+  __structuredAttrs = true;
   nativeBuildInputs = [
     autoreconfHook
     pkg-config
@@ -77,7 +80,6 @@ stdenv.mkDerivation (finalAttrs: {
   ]
   ++ lib.optional enableCddb libcddb
   ++ lib.optional enableSrc libsamplerate
-  ++ lib.optional enableIfp (throw "unsupported option")
   ++ lib.optional enableLua lua
 
   ++ lib.optional enableSndfile libsndfile
@@ -91,7 +93,7 @@ stdenv.mkDerivation (finalAttrs: {
   ++ lib.optional enableLame lame
   ++ lib.optional enableMod libmodplug
   ++ lib.optional enableMpc libmpcdec
-  ++ lib.optional enableMac mac
+  ++ lib.optional enableMac monkeys-audio
   ++ lib.optional enableWavpack wavpack
   ++ lib.optional enableLavc ffmpeg
 
@@ -112,4 +114,23 @@ stdenv.mkDerivation (finalAttrs: {
       url = "https://github.com/jeremyevans/aqualung/commit/d830ac5898412280ea02faebe82509a8129dac59.patch";
       hash = "sha256-YMWlfK4242q3DoCcmfIeP3xQk2Ot05ifs2AN5CCDcco=";
     });
+  passthru.updateScript = nix-update-script { };
+  meta = {
+    homepage = "https://aqualung.jeremyevans.net/";
+    description = "Gapless music player";
+    license = lib.licenses.gpl2Plus;
+    mainProgram = "aqualung";
+    maintainers = with lib.maintainers; [ amiryal ];
+    platforms = lib.platforms.all;
+
+    # Source for longDescription: https://github.com/jeremyevans/aqualung/blob/5e0226f7ddd871be4fc4f0854a5e125bde3d7918/index.md
+    # (Converted with html2markdown from nixpkgs#html2text.)
+    longDescription = ''
+      Aqualung is an advanced music player originally targeted at the GNU/Linux
+      operating system, today also running on FreeBSD and OpenBSD, with native ports
+      to Mac OS X and even Microsoft Windows. It plays audio CDs, internet radio
+      streams and podcasts as well as soundfiles in just about any audio format and
+      has the feature of _**inserting no gaps**_ between adjacent tracks.
+    '';
+  };
 })
